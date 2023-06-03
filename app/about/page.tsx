abff71281026721c1eb76375755991189a4ae765
caltistals <y.db.qp.y@gmail.com>
2023-06-03 18:54:59 +0900

    fix: import

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,14 +2,14 @@ import { PageTitle } from "../_components/PageTitle";
 import { Box } from "@/libs";
 import { Profile } from "../_components/Profile";
 import { SkillsSection } from "./_components/SkillsSection";
-import { WorksSection } from "./_components/WorksSection";
+import { CareerSection } from "./_components/CareerSection";
 
 export default function StaticPage() {
   return (
     <Box as="main">
       <PageTitle title="About" />
       <Profile />
-      <WorksSection />
+      <CareerSection />
       <SkillsSection />
     </Box>
   );
