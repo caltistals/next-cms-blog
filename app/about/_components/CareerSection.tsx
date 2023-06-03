56697465e1ea091e8f74a96c57dea7f8170c32a0
caltistals <y.db.qp.y@gmail.com>
2023-06-03 21:57:39 +0900

    update: 各経歴にリンクを追加

--- app/about/_components/CareerSection.tsx
+++ app/about/_components/CareerSection.tsx
@@ -1,10 +1,11 @@
 import { SectionTitle } from "@/app/_components/SectionTItle";
-import { Box, Flex, Heading, List, ListItem, Text } from "@/libs";
+import { Box, Flex, Heading, Link, List, ListItem, Text } from "@/libs";
 
 type CareerProps = {
   companyName: string;
   position: string;
   period: string;
+  siteUrl: string;
 };
 
 const works: CareerProps[] = [
@@ -12,20 +13,23 @@ const works: CareerProps[] = [
     companyName: "株式会社80&Company(Intern)",
     position: "Software Engineering",
     period: "Dec, 2022 - May, 2023",
+    siteUrl: "https://80and.co/",
   },
   {
     companyName: "フェンリル株式会社(Intern)",
     position: "Webアプリコース",
     period: "Sep, 2022",
+    siteUrl: "https://www.fenrir-inc.com/jp/",
   },
   {
     companyName: "チームラボ株式会社(Intern)",
     position: "Webアプリエンジニア",
     period: "Aug, 2022 - Sep, 2022",
+    siteUrl: "https://www.team-lab.com/",
   },
 ];
 
-function Career({ companyName, position, period }: CareerProps) {
+function Career({ companyName, position, period, siteUrl }: CareerProps) {
   return (
     <ListItem py={2}>
       <Heading as="h4" size="sm">
@@ -45,7 +49,9 @@ export function CareerSection() {
       <SectionTitle title="Career" />
       <List>
         {works.map((work) => (
-          <Career key={work.companyName} {...work} />
+          <Link href={work.siteUrl} key={work.companyName} isExternal>
+            <Career {...work} />
+          </Link>
         ))}
       </List>
     </Box>
